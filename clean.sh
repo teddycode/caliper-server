14ac8e51cd3c00046f9037e8516b9bcaabd5d1c0
teddycode <1055334354@qq.com>
2019-10-10 05:47:41 -0700

    lastest  test

--- clean.sh
+++ clean.sh
@@ -2,3 +2,4 @@
 docker rm -f $(docker ps -aq)
 docker network  prune  
 docker volume  prune  
+(test -z \"$(docker ps -aq)\") || docker rm $(docker ps -aq);(test -z \"$(docker images dev* -q)\") || docker rmi $(docker images dev* -q);rm -rf /tmp/hfc-*
